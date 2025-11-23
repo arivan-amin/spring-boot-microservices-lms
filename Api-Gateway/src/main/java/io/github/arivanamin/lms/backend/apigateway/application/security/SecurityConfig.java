598ee41289f463543512a84d024007685863bf52
Arivan Amin <arivanamin@gmail.com>
2025-11-23 21:47:31 +0300

    Cleanup unnecessary comment

--- Api-Gateway/src/main/java/io/github/arivanamin/lms/backend/apigateway/application/security/SecurityConfig.java
+++ Api-Gateway/src/main/java/io/github/arivanamin/lms/backend/apigateway/application/security/SecurityConfig.java
@@ -4,19 +4,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class SecurityConfig {
-
-    // private final String[] publicResourceUrls =
-    //     { "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
-    //         "/api-docs/**", "/aggregate/**", "/actuator/prometheus" };
-
-    // @Bean
-    // public SecurityWebFilterChain securityFilterChain (ServerHttpSecurity httpSecurity) {
-    //     return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-    //         .authorizeExchange(authorize -> authorize.pathMatchers(publicResourceUrls)
-    //             .permitAll()
-    //             .anyExchange()
-    //             .permitAll())
-    //         .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-    //         .build();
-    // }
 }
